3861edf753e04d7d58f81162daa3a21287170018
Shu-Wei Yeh <42593886+marit760114@users.noreply.github.com>
2022-08-05 18:16:18 +1000

    Add files via upload

---
+++ WebApi04082022/Models/Sale.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi04082022.Models
+{
+    public class Sale
+    {
+        public int ID { get; set; }
+        public int ProductId { get; set; }
+        public int CustomerId { get; set; }
+        public int StoreId { get; set; }
+        public DateTime DateSold { get; set; }
+    }
+}
---
+++ WebApi04082022/Models/Store.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi04082022.Models
+{
+    public class Store
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
